9682058fff8bb54038cc9a75395d7471cb7d1e59
karlorg <karlorg@users.noreply.github.com>
2015-01-03 13:14:50 +0000

    tiny delinting in config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../db.sqlite')
 
-SECRET_KEY = ':\x8dkR\xf9\x05\xc2\xd2,\xd4t\x0f\x0bvB\xbb\x1a.\xce\xbd\x0b\x17\xdc\xb7'
+SECRET_KEY = ':\x8dkR\xf9\x05\xc2\xd2,\xd4t\x0f\x0bvB\xbb\x1a.\xce\xbd\x0b\x17\xdc\xb7'  # noqa
