d8371105a9a3addd002709e0feedb55643a5fa1e
karlorg <karlorg@users.noreply.github.com>
2015-01-15 16:27:26 +0000

    some JS de-linting and re-styling

--- app/static/persona.js
+++ app/static/persona.js
@@ -29,42 +29,42 @@
             loggedInUser = null;
         }
 
-        navigator.id.watch({
-            loggedInUser: loggedInUser,
-            onlogin: function(assertion) {
+        navigator.id.watch(
+            { loggedInUser: loggedInUser
+            , onlogin: function(assertion) {
                 // A user has logged in! Here you need to:
                 // 1. Send the assertion to your backend for verification and
                 // to create a session.  2. Update your UI.
-                $.ajax({ 
-                    type: 'POST',
-                    url: '/persona/login', // This is a URL on your website.
-                    data: {assertion: assertion},
-                    success: function(res, status, xhr) { 
+                $.ajax(
+                    { type: 'POST'
+                    , url: '/persona/login' // This is a URL on your website.
+                    , data: {assertion: assertion}
+                    , success: function(res, status, xhr) {
                         window.location.reload();
-                    },
-                    error: function(xhr, status, err) {
+                    }
+                    , error: function(xhr, status, err) {
                         navigator.id.logout();
                         // alert("Login failure: " + err);
                     }
                 });
-            },
-            onlogout: function() {
+            }
+            , onlogout: function() {
                 // A user has logged out! Here you need to:
                 // Tear down the user's session by redirecting the user or
                 // making a call to your backend.  Also, make sure loggedInUser
                 // will get set to null on the next page load.  (That's a
                 // literal JavaScript null. Not false, 0, or undefined. null.)
-                $.ajax({
-                    type: 'POST',
-                    url: '/logout', // This is a URL on your website.
-                    success: function(res, status, xhr) { 
+                $.ajax(
+                    { type: 'POST'
+                    , url: '/logout' // This is a URL on your website.
+                    , success: function(res, status, xhr) {
                         window.location.reload();
-                    },
-                    error: function(xhr, status, err) {
+                    }
+                    , error: function(xhr, status, err) {
                         // alert("Logout failure: " + err);
                     }
                 });
-            },
+            }
         });
     };
 
--- app/static/tests/tests.js
+++ app/static/tests/tests.js
@@ -7,30 +7,29 @@
         var logout_called = false;
         var watch_called = false;
         var mock_navigator = {
-            id: {
-                logout: function () {
+            id: { logout: function() {
                     logout_called = true;
-                },
-                request: function() {
+                }
+                , request: function() {
                     request_called = true;
-                },
-                watch: function (params) {
+                }
+                , watch: function (params) {
                     var pUser = params.loggedInUser;
                     var tUser = tesuji_charm.current_persona_email;
-                    ok((tUser === "" && pUser === null)
-                        || (tUser !== "" && pUser === tUser),
+                    ok((tUser === "" && pUser === null) ||
+                        (tUser !== "" && pUser === tUser),
                         "n.i.watch passed good loggedInUser");
 
                     var isFunction = function (functionToCheck) {
                         var getType = {};
                         return functionToCheck && 
                             (getType.toString.call(functionToCheck) ===
-                             '[object Function]');
-                    }
+                            '[object Function]');
+                    };
                     ok(isFunction(params.onlogin),
-                            "n.i.watch passed function for onlogin");
+                        "n.i.watch passed function for onlogin");
                     ok(isFunction(params.onlogout),
-                            "n.i.watch passed function for onlogout");
+                        "n.i.watch passed function for onlogout");
 
                     watch_called = true;
                 }
@@ -39,7 +38,7 @@
         equal(watch_called, false);
         tesuji_charm.current_persona_email = "";
         tesuji_charm.persona.initialize(mock_navigator);
-        equal(watch_called, true, "navigator.id.watch called")
+        equal(watch_called, true, "navigator.id.watch called");
         tesuji_charm.current_persona_email = "bob@example.com";
         tesuji_charm.persona.initialize(mock_navigator);
 
