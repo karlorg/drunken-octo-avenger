91b3ccd3fcc8139c1df45bf405c0f76a1ac4cfff
allanderek <allan.clark@gmail.com>
2015-03-30 16:23:14 +0100

    Updated the server player test to actually check the status of the server player and check that  moves on the game created. This test of course currently fails.

--- app/tests/test_server_player.py
+++ app/tests/test_server_player.py
@@ -9,11 +9,17 @@ import unittest
 from .. import main
 
 class TestServerPlayer(unittest.TestCase):
+    def assert_status_list_lengths(self, email, your_turns, not_your_turns):
+        main.get_player_games(email)
+        your_turn_games, not_your_turn_games = main.get_status_lists(email)
+        self.assertEqual(len(your_turn_games), your_turns)
+        self.assertEqual(len(not_your_turn_games), not_your_turns)
     def test_server_player(self):
         test_player_email = "serverplayer@localhost"
         test_opponent_email = "serverplayermock@localhost"
 
         main.clear_games_for_player_internal(test_player_email)
         main.create_game_internal(test_player_email, test_opponent_email)
-        number_games = main.server_player_act(test_player_email)
-        self.assertEqual(number_games, 1)
+        self.assert_status_list_lengths(test_player_email, 1, 0)
+        main.server_player_act(test_player_email)
+        self.assert_status_list_lengths(test_player_email, 0, 1)
