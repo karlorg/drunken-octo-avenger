b309f1c3bc405f245b4ceadb381733a655a87064
allanderek <allan.clark@gmail.com>
2016-03-22 23:28:15 +0000

    Removed the future and builtins boilerplate at the head of most python files that was only there for Python2, support for which we are now dropping.

--- app/go_rules.py
+++ app/go_rules.py
@@ -1,9 +1,3 @@
-from __future__ import (
-        absolute_import, division, print_function, unicode_literals)
-from builtins import (ascii, bytes, chr, dict, filter, hex, input,  # noqa
-                      int, map, next, oct, open, pow, range, round,
-                      str, super, zip)
-
 from collections import namedtuple
 from enum import IntEnum
 
--- app/tests/test.py
+++ app/tests/test.py
@@ -1,9 +1,3 @@
-from __future__ import (
-        absolute_import, division, print_function, unicode_literals)
-from builtins import (ascii, bytes, chr, dict, filter, hex, input,  # noqa
-                      int, map, next, oct, open, pow, range, round,
-                      str, super, zip)
-
 from contextlib import contextmanager
 from itertools import chain
 from mock import ANY, Mock, patch
