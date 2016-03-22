a010b9baa8c53894052e4d3cbf063dd79bc693f0
allanderek <allan.clark@gmail.com>
2016-03-22 23:12:26 +0000

    Modify develop.sh to assume Python 3.

--- develop.sh
+++ develop.sh
@@ -7,21 +7,16 @@ then
 VENV=$1
 else
     # If it is not specified on the command line we check for the existance
-    # of, first a ../venv3.4 and then a ../venv2.7 directory, if either exists
-    # we set that as the virtual environment otherwise we complain and exit.
+    # of a ../venv3.4 directory, if that exists we set that as the virtual
+    # environment otherwise we complain and exit.
     if [ -d "../venv3.4" ]
     then
     VENV=../venv3.4
     else
-        if [ -d "../venv2.7" ]
-        then
-        VENV=../venv2.7
-        else
-            echo "I don't know where your virtual environment is"
-            echo "Perhaps you need to run setup.sh?"
-            echo "Otherwise please specify your venv"
-            return 1
-        fi
+        echo "I don't know where your virtual environment is"
+        echo "Perhaps you need to run setup.sh?"
+        echo "Otherwise please specify your venv"
+        return 1
     fi
 fi
 
